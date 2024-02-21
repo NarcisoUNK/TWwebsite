7badf3e89c4648468976b57ff27b5ea000502d92
Unknown Narciso <24473@stu.ipbeja.pt>
2024-02-21 14:44:36 +0000

    Navbar1

--- navbar.js
+++ navbar.js
@@ -1,11 +1,15 @@
 function toggleNav() {
     var sidebar = document.getElementById("sidebar");
     var content = document.querySelector(".content");
+    var closeBtn = document.querySelector(".close-btn");
+    
     if (sidebar.style.width === "200px") {
         sidebar.style.width = "0";
         content.style.marginLeft = "0";
-    } else {
+        closeBtn.innerText = "Open Navbar";
+    } else 
         sidebar.style.width = "200px";
         content.style.marginLeft = "220px";
-    }
+        closeBtn.innerText = "Close Navbar";
+    
 }
